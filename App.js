740d2a7b03f75df939c936f54cac1935d39cae00
jagrit2008 <70403360+jagrit2008@users.noreply.github.com>
2021-02-16 23:51:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import TransactionScreen from './screens/ScanScreen';
